fb858dc392ee2fe10fabda49d655d51c5a2d46cf
kpeis695 <kpeis695@gmail.com>
2025-06-09 22:40:10 -0400

    Initial Commit

---
+++ RoadBuddy/RoadBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadBuddyApp.swift
+//  RoadBuddy
+//
+//  Created by Sylvester Kpei on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RoadBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoadBuddyTests/RoadBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoadBuddyTests.swift
+//  RoadBuddyTests
+//
+//  Created by Sylvester Kpei on 6/9/25.
+//
+
+import Testing
+@testable import RoadBuddy
+
+struct RoadBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
